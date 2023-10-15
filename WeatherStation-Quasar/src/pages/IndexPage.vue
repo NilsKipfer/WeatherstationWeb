3ca51c1a64d700623c2a004a7b892622676fe1ec
NilsKipfer <nils_k_93@hotmail.ch>
2023-10-15 16:04:53 +0200

    add design

--- WeatherStation-Quasar/src/pages/IndexPage.vue
+++ WeatherStation-Quasar/src/pages/IndexPage.vue
@@ -1,16 +1,30 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="">
+      <q-banner class="bg-secondary">
+        <q-banner class="bg-primary">
+          <q-banner class="bg-secondary text-white">
+            <p><center><q-icon name="signal_wifi_off" color="info" /></center></p>
+            <p><center>{{26}} {{ "° C" }}</center></p>
+            <p><center>{{ "Temperature" }}</center></p>
+          </q-banner>
+        </q-banner>
+        <q-banner class="bg-primary">
+          <q-banner class="bg-secondary text-white">
+            <p><center><q-icon name="signal_wifi_off" color="info" /></center></p>
+            <p><center>{{26}} {{ "%" }}</center></p>
+            <p><center>{{ "Humidity" }}</center></p>
+          </q-banner>
+        </q-banner>
+      </q-banner>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-
+var temp = "° C";
+var hum = "%";
 export default defineComponent({
   name: 'IndexPage'
 })
